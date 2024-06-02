0de0e5560c0fce139ac1b96455840e3d5c6748b5
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-06-02 06:46:21 +0000

    [LeetCode Sync] Runtime - 62 ms (21.01%), Memory - 63.7 MB (99.98%)

--- leetcode-solutions/0287-find-the-duplicate-number/solution.cpp
+++ leetcode-solutions/0287-find-the-duplicate-number/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+//method 1-
+    int duplicateM1(vector<int>&nums){
         int b=0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++){
@@ -10,5 +11,27 @@ public:
             }
         }
     return b;
+    }
+    // negative marking method . it has lower time complexity
+    //we will mark nums[num[i]] visited by marking it negative and if number comes out to be negative , that is duplicate number.
+    int NegativeMarkingMethod(vector<int>&nums){
+        int ans=-1;
+        for(int i=0;i<nums.size();i++){
+            int index=abs(nums[i]);
+            //already visited
+            if(nums[index]<0){
+                ans= index;
+                break;
+            }
+            //marking visited
+            else{
+                nums[index]*=-1;
+            }
+        }
+        return ans;
+    }
+    int findDuplicate(vector<int>& nums) {
+      int ans= NegativeMarkingMethod(nums);
+      return ans;
     }
 };
