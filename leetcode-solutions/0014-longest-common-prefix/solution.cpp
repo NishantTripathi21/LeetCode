87b3c2979b6f8d16cde615dcbea25b52563160c0
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-07-11 12:41:25 +0000

    [LeetCode Sync] Runtime - 12 ms (5.22%), Memory - 18.4 MB (5.14%)

---
+++ leetcode-solutions/0014-longest-common-prefix/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        int i=0;
+        while(true){
+            char cur_char=0;
+            for(auto str: strs){
+                if(i>=str.size()){
+                    cur_char=0;
+                    break;
+                }
+                if(cur_char==0){
+                    cur_char=str[i];
+
+                }
+                else if(str[i]!=cur_char){
+                    cur_char=0;
+                    break;
+                }
+
+            }
+             if(cur_char==0){
+                break;
+            }
+            ans.push_back(cur_char);
+            i++;
+           
+        }
+        return ans;
+        
+    }
+};
