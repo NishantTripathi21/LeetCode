01674ddc8c26084a142f2802c8838fa149cfd362
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-07-14 04:30:20 +0000

    [LeetCode Sync] Runtime - 3 ms (49.97%), Memory - 9.3 MB (99.95%)

--- leetcode-solutions/0242-valid-anagram/solution.cpp
+++ leetcode-solutions/0242-valid-anagram/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool method2(string s, string t){
+        // this method is called solution by hashing , we created hash table(array of 267 elements in which every ascii value has its share)
         int maps[267]={0};
         for(int i=0;i<s.size();i++){
             maps[s[i]]++;
