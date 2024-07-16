f9bc9c01470f911ea9cd336bfe9c6e8f9506d7e8
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-07-16 20:13:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.8 MB (95.07%)

---
+++ leetcode-solutions/0443-string-compression/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int count=1;
+        int index=0;
+        int prev=chars[0];
+        for(int i=1;i<chars.size();++i){
+            if(chars[i]==prev){
+                count++;
+            }
+            else{
+                chars[index++]=prev;
+                if(count>1){
+                    int start=index;
+                    while(count){
+                        chars[index++]=count%10 + '0';
+                        count=count/10;
+                        }
+                        reverse(chars.begin()+start,chars.begin()+index);
+                }
+                prev=chars[i];
+                count=1;
+            }
+        }
+        chars[index++]=prev;
+        if(count>1){
+            int start=index;
+             while(count){
+                        chars[index]=(count%10) + '0';
+                        index++;
+                        count=count/10;
+                        }
+                        reverse(chars.begin()+start,chars.begin()+index);
+                }
+        
+        return index ;
+
+    }
+};
