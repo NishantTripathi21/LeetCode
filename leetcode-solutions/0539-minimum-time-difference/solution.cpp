8d07d1a13c8b0e07b31b10f83cbe562e11a64591
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-07-18 06:10:59 +0000

    [LeetCode Sync] Runtime - 11 ms (13.21%), Memory - 18.7 MB (22.40%)

---
+++ leetcode-solutions/0539-minimum-time-difference/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int convertToMin(string &str){
+        int totalMins=0;
+        int hour=stoi(str.substr(0,2));
+        int min=stoi(str.substr(3,2));
+        totalMins=hour*60+min;
+        return totalMins;
+    }
+    int findMinDifference(vector<string>& timePoints) {
+        vector<int>mins;
+        for(auto str: timePoints){
+            mins.push_back(convertToMin(str));
+
+        }
+        sort(mins.begin(),mins.end());
+        int mini=INT_MAX;
+        for(int i=0;i<mins.size()-1;i++){
+            mini=min(mini,(mins[i+1]-mins[i]));
+        }
+        int reverser_ans=1440+mins[0] - mins[mins.size()-1];
+        mini=min(mini,reverser_ans);
+        return mini;
+
+    }
+};
