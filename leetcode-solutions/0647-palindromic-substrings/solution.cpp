69b0f6406d36208c0e9f10619014b8d8b2bfa280
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-01 16:33:56 +0000

    [LeetCode Sync] Runtime - 1315 ms (8.59%), Memory - 407.1 MB (9.06%)

---
+++ leetcode-solutions/0647-palindromic-substrings/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int i, int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    int countSubstrings(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                if(isPalindrome(s,i,j)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+        
+        
+    }
+};
