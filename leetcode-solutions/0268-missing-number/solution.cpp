2e2597471d81dc88a9f0582ccd3d10e3b0c0e245
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-02 10:04:07 +0000

    [LeetCode Sync] Runtime - 12 ms (11.48%), Memory - 20.6 MB (99.87%)

--- leetcode-solutions/0268-missing-number/solution.cpp
+++ leetcode-solutions/0268-missing-number/solution.cpp
@@ -1,44 +1,12 @@
 class Solution {
 public:
-    int missingNumberUsingOtherArray(vector<int>&nums){
-         int missing;
-        // sort the array
-        // make a new array with all elements upto n
-        // compare both arrays with corner case like exaple two
-        sort(nums.begin(), nums.end()); 
-        vector<int>brr(nums.size()+1,0);
-        for(int i=0;i<=nums.size();i++){
-            brr[i]=i;
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int  sum=n*(n+1)/2;
+        for(int i=0;i<n;i++){
+            sum-=nums[i];
         }
-        if(nums.back()!=brr.back()){
-             missing= brr.back();
-         }
-         else{
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=brr[i]){
-                missing = brr[i];
-                break;
-            }
-         }
-         }
-    return missing;
+        return sum;
+        
     }
-
-    int MissingElementUsingXor(vector<int>&nums){
-        int ans=0;
-        //Xor all elements of Array
-        for(int i=0;i<nums.size();i++){
-            ans^=nums[i];
-        }
-        //xor all range items from [0,N]
-        for(int i=0;i<=nums.size();i++){
-            ans^=i;
-        }
-        return ans;
-    }
-
-
-    int missingNumber(vector<int>& nums){
-      return  MissingElementUsingXor(nums);
-           } 
 };
