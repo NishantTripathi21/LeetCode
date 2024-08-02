368a80a23acce3ee3ab29d94362c2cf7c4cef1c9
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-02 14:02:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.4 MB (99.98%)

---
+++ leetcode-solutions/0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>arr;
+        int i=0;
+        int j=0;
+        while(i<m && j<n){
+            if(nums1[i]>nums2[j]){
+                arr.push_back(nums2[j]);
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                arr.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i]==nums2[j]){
+                arr.push_back(nums1[i]);
+                i++;
+            }
+        }
+        while (i < m) {
+        arr.push_back(nums1[i]);
+        i++;
+    }
+
+    // Add remaining elements from nums2, if any
+    while (j < n) {
+        arr.push_back(nums2[j]);
+        j++;
+    }
+        for(int k=0;k<arr.size();k++){
+            nums1[k]= arr[k];
+        }
+       // nums1=arr;
+    }
+};
