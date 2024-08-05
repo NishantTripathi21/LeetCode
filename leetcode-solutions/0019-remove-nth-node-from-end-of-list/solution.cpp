5ec5048d066db11d2e85b35d0c8a834786b72d65
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-05 16:43:01 +0000

    [LeetCode Sync] Runtime - 8 ms (0.21%), Memory - 14.8 MB (99.75%)

---
+++ leetcode-solutions/0019-remove-nth-node-from-end-of-list/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int findLen(ListNode*&head){
+        ListNode*temp=head;
+        int len=0;
+        while(temp!=NULL){
+            len++;
+            temp=temp->next;
+        }
+        return len;
+    }
+    void deleteNode(ListNode*&head,int position){
+        if(head==NULL)return;
+        int len=findLen(head);
+        //single element LL
+        if(head->next==NULL){
+            delete head;
+            head=NULL;
+        }
+        else if(position==1){
+            //delete from head
+            ListNode* temp=head;
+            head=head->next;
+            temp->next=NULL;
+            delete temp;
+
+        }
+        else if(position==len){
+            ListNode* temp=head;
+            ListNode* temp1=temp->next;
+            while(temp1->next!=NULL){
+                temp=temp->next;
+                temp1=temp1->next;
+            }
+            temp->next=NULL;
+
+           delete temp1;
+
+        }
+    
+        //delete from any pos
+        else{
+            ListNode*prev=NULL;
+            ListNode*curr=head;
+            while(position!=1){
+                position--;
+                prev=curr;
+                curr=curr->next;
+            }
+            //prev ke next me curr ka next add karoo;
+            prev->next=curr->next;
+            //node isolate krdo
+            curr->next=NULL;
+            //delete node;
+            delete curr;
+
+        }
+
+    }
+
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int len=findLen(head);
+        int position=len-n+1;
+        deleteNode(head,position);
+        return head;
+    }
+};
