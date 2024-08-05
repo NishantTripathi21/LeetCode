5073ca02c706863a14a2a8525f42ada19aceea25
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-05 09:08:36 +0000

    [LeetCode Sync] Runtime - 7 ms (82.36%), Memory - 10.9 MB (99.98%)

---
+++ leetcode-solutions/0141-linked-list-cycle/solution.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycleM2(ListNode* head){
+        ListNode* temp=head;
+        map<ListNode*, bool>table;
+        while(temp!=NULL){
+            if(table[temp]==false){
+                table[temp]=true;
+            }
+            else {
+                return true;
+            }
+            temp=temp->next;
+        }
+        return false;
+    }
+
+    bool hasCycle(ListNode *head) {
+        //we can solve this question by using map and tortois method
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+            fast=fast->next;   
+            slow=slow->next;           
+            }
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+       // return hasCycleM2(head);
+
+    }
+};
