cbd4f35286073c3024ad78c5bde1a5012aacdb4c
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-14 18:37:25 +0000

    [LeetCode Sync] Runtime - 149 ms (5.19%), Memory - 91.5 MB (22.81%)

---
+++ leetcode-solutions/1582-design-browser-history/solution.cpp
@@ -0,0 +1,51 @@
+class BrowserHistory {
+    stack<string>browserStack,fwdStack;
+public:
+    BrowserHistory(string homepage) {
+        browserStack.push(homepage);
+    }
+    
+    void visit(string url) {
+        //clear fwd history
+         while(!fwdStack.empty()){
+            fwdStack.pop();
+         }
+        browserStack.push(url);
+    }
+    
+    string back(int steps) {
+        while(steps--){
+            if(browserStack.size()>1){
+                fwdStack.push(browserStack.top());
+                browserStack.pop();
+            }
+            else{
+                //back isnt posible
+                break;
+            }
+        }
+        return browserStack.top();
+    }
+    
+    string forward(int steps) {
+        while(steps--){
+            if(!fwdStack.empty()){
+                browserStack.push(fwdStack.top());
+                fwdStack.pop();
+            }
+            else{
+                break;
+            }
+        }
+        return browserStack.top();
+        
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
