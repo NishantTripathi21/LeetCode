faba32f8fee46b2f221967818d2860ee0d96e738
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-14 13:01:38 +0000

    [LeetCode Sync] Runtime - 7 ms (9.76%), Memory - 9.1 MB (99.98%)

---
+++ leetcode-solutions/0032-longest-valid-parentheses/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st;
+       int maxLen=0;
+        st.push(-1);
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(ch=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    int len=i-st.top();
+                    maxLen=max(len,maxLen);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
