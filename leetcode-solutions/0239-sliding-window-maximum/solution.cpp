1f45af200124e2fa10b547a155712de6d8111a29
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-20 15:56:22 +0000

    [LeetCode Sync] Runtime - 157 ms (11.86%), Memory - 137.5 MB (94.89%)

--- leetcode-solutions/0239-sliding-window-maximum/solution.cpp
+++ leetcode-solutions/0239-sliding-window-maximum/solution.cpp
@@ -25,13 +25,13 @@ public:
         // remove
         if (i - dq.front() >= k)
             dq.pop_front();
-        int ele = nums[i];
+            int ele = nums[i];
         // push elements in the queue and remove chote vale elements
-        while (!dq.empty() && ele > nums[dq.back()])
-        {
+            while (!dq.empty() && ele > nums[dq.back()])
+            {
             dq.pop_back();
-        }
-        dq.push_back(i);
+            }
+            dq.push_back(i);
     }
     // handle last window
     ans.push_back(nums[dq.front()]);
