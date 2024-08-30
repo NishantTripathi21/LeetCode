f93cd05e35b5d31743371b116f27ebcb7b424a97
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-08-30 20:45:17 +0000

    [LeetCode Sync] Runtime - 483 ms (5.05%), Memory - 108.8 MB (25.90%)

--- leetcode-solutions/2094-remove-stones-to-minimize-the-total/solution.cpp
+++ leetcode-solutions/2094-remove-stones-to-minimize-the-total/solution.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    class comp{
-        public:
-        bool operator()(int a,int b){
-            return a > b;
-        }
-    };
     int minStoneSum(vector<int>& piles, int k) {
+        //initiate max heap so that we can operate on maximum element.
+        //operation on maximum elelemt will give us minimum result
         priority_queue<int>pq;
         for(auto i:piles){
+            //push elements in max heap/
             pq.push(i);
         }
         while(k){
+            //operate k number of times
             k--;
             auto temp=pq.top();
             pq.pop();
             pq.push(temp-temp/2);
         }
         int sum=0;
+        //sum each element
         while(!pq.empty()){
             sum+=pq.top();
             pq.pop();
