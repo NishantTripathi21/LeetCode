465c12349a46fa9415f7e4b9e2c2a821249a8fcc
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-09-21 09:28:09 +0000

    [LeetCode Sync] Runtime - 3 ms (54.15%), Memory - 16.5 MB (31.82%)

--- leetcode-solutions/0039-combination-sum/solution.cpp
+++ leetcode-solutions/0039-combination-sum/solution.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     void combination_helper(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&nums,int index){
+        if(index >= candidates.size())return;
         if(target==0){
             ans.push_back(nums);
             return;
         }
         if(target<0) return;
-        for(int i=index ; i<candidates.size();i++){
-            nums.push_back(candidates[i]);
-            combination_helper(candidates,target-candidates[i],ans,nums,i);
-            //backtracking
-            nums.pop_back();
-        }
+        //here we used loop becuse only include nd exclude pattern will not work
+        //exclude
+        combination_helper(candidates,target,ans,nums,index+1);
+        //include
+        nums.push_back(candidates[index]);
+        combination_helper(candidates,target - candidates[index],ans,nums,index);
+        //bcktrack
+        nums.pop_back();
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
