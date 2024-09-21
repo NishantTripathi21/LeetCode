851e7ce20c0bd60c6d28ceeccad4d27296942b85
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-09-21 09:14:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (21.86%)

--- leetcode-solutions/0216-combination-sum-iii/solution.cpp
+++ leetcode-solutions/0216-combination-sum-iii/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    void usingRec(int num, int k , int n, vector<vector<int>>&ans,vector<int>temp){
+    void usingRec(int num, int k , int n, vector<vector<int>>&ans,vector<int>&temp){
         if(k < 0)return ;
         if(num > 10)return;
-        cout<<" K: "<<k<<" n: "<<n<<" num: "<<num<<endl;
+        // cout<<" K: "<<k<<" n: "<<n<<" num: "<<num<<endl;
         if(k==0 && n==0){
             ans.push_back(temp);
             return;
@@ -14,6 +14,7 @@ public:
         //include
         temp.push_back(num);
         usingRec(num+1, k-1, n - num, ans,temp);
+        temp.pop_back();
         
     }
     vector<vector<int>> combinationSum3(int k, int n) {
