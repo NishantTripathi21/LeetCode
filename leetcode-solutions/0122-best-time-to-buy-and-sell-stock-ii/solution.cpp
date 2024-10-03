d59436d066ed19afe9b4d7f5aa8588857e0f3320
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-03 19:14:25 +0000

    [LeetCode Sync] Runtime - 13 ms (5.49%), Memory - 20.6 MB (15.46%)

--- leetcode-solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ leetcode-solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    //another mthod by using include exclude
+    int usingRec2(vector<int>&prices,int index,int buy,vector<vector<int>>&dp){
+        if(index==prices.size()){
+            return 0;
+        }
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
+        }
+        int profit=0;
+        if(buy){
+            //when you can buy. you try buy on the index day or skip the index day
+            profit=max(-prices[index] + usingRec2(prices,index+1,0,dp),0+ usingRec2(prices,index+1,1,dp));
+        }
+        else{
+            //if u have already bought,you sell on index day, or you may skip and sell other day
+            profit=max( prices[index] + usingRec2(prices,index+1,1,dp) , 0+ usingRec2(prices,index+1,0,dp));
+        }
+        return dp[index][buy]=profit;
+    }
+
     int usingRec(vector<int>&prices,int buyDay,vector<int>&dp){
         if(buyDay==prices.size()-1){
             return 0;
@@ -23,9 +43,11 @@ public:
         }
         return dp[0];
     }
+    //no spaceOptmisation is possible
     int maxProfit(vector<int>& prices) {
         //vector<int>dp(prices.size()+1,-1);
         //return usingRec(prices,0,dp);
-        return usingTabulation(prices);
+        vector<vector<int>>dp(prices.size()+1,vector<int>(3,-1));
+        return usingRec2(prices,0,1,dp);
     }
 };
