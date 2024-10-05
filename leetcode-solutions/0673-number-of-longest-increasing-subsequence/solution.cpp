98bc92f714c4d104c632ae65ac1d55398fdab525
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-05 10:51:58 +0000

    [LeetCode Sync] Runtime - 35 ms (38.92%), Memory - 16.2 MB (100.00%)

---
+++ leetcode-solutions/0673-number-of-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    //maintain a count array to store in frequency of LIS
+    int numberOf_LIS(vector<int>& nums){
+        vector<int>dp(nums.size(),1);
+        vector<int>count(nums.size(),1);
+        int maxi=1;
+        for(int index=0;index<nums.size();index++){
+            for(int prev=0; prev<index; prev++){
+                if(nums[index] > nums[prev] && dp[index] < dp[prev] +1){
+                    dp[index]=1+ dp[prev];
+                    count[index]=count[prev];
+                }
+                else if(nums[index] > nums[prev] && dp[index] == dp[prev] +1){
+                    count[index]=count[prev]+count[index];
+                }
+            }
+            maxi=max(maxi,dp[index]);
+        }
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(dp[i]==maxi){
+                ans=ans+count[i];
+            }
+        }
+        return ans;
+    }
+    int findNumberOfLIS(vector<int>& nums) {
+        return numberOf_LIS(nums);
+    }
+};
