7f3029f61c2d06044802f54db23aa6fffeb15243
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-12 18:30:29 +0000

    [LeetCode Sync] Runtime - 7 ms (6.58%), Memory - 21.9 MB (99.94%)

--- leetcode-solutions/0110-balanced-binary-tree/solution.cpp
+++ leetcode-solutions/0110-balanced-binary-tree/solution.cpp
@@ -17,16 +17,29 @@ public:
         int right=height(root->right);
         return max(left,right)+1;
     }
+    bool isBalanced1=true;
+    int heightModified(TreeNode*root){
+        if(!root)return 0;
+        int leftH=heightModified(root->left);
+        int rightH=heightModified(root->right);
+        int ans=max(leftH,rightH)+1;
+        if(abs(leftH - rightH) > 1 && isBalanced1) {
+            isBalanced1 = false;
+        }
+        return ans;
+    }
     bool isBalanced(TreeNode* root) {
-        if(!root)return true;
-        // current node
-        int leftHeight=height(root->left);
-        int rightHeight=height(root->right);
-        int diff=abs(leftHeight-rightHeight);
-        bool currNodeAns=diff<=1;
-        //recussive calls
-        bool left=isBalanced(root->left);
-        bool right=isBalanced(root->right);
-        return left&&right&&currNodeAns?true:false;
+        // if(!root)return true;
+        // // current node
+        // int leftHeight=height(root->left);
+        // int rightHeight=height(root->right);
+        // int diff=abs(leftHeight-rightHeight);
+        // bool currNodeAns=diff<=1;
+        // //recussive calls
+        // bool left=isBalanced(root->left);
+        // bool right=isBalanced(root->right);
+        // return left&&right&&currNodeAns?true:false;
+        heightModified(root);
+        return isBalanced1;
     }
 };
