724871b278d0815dbd1e7e8664f963822d1eba30
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-12 18:15:17 +0000

    [LeetCode Sync] Runtime - 15 ms (21.07%), Memory - 22.5 MB (99.97%)

--- leetcode-solutions/0543-diameter-of-binary-tree/solution.cpp
+++ leetcode-solutions/0543-diameter-of-binary-tree/solution.cpp
@@ -11,19 +11,34 @@
  */
 class Solution {
 public:
+    int d=0;
     int height(TreeNode*root){
         if(!root)return 0;
         int leftans=height(root->left);
         int rightans=height(root->right);
         return max(leftans,rightans)+1;
     }
-    int diameterOfBinaryTree(TreeNode* root) {
+    int modifiedHeight(TreeNode*root){
         if(!root) return 0;
+
+        int leftHeight=modifiedHeight(root->left);
+        int rightHeight=modifiedHeight(root->right);
+        int ans=max(leftHeight,rightHeight)+1;
+        d=max(d,leftHeight+rightHeight);
+        return ans;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        // if(!root) return 0;
         
-        int leftSideAns=diameterOfBinaryTree(root->left);
-        int rightSideAns=diameterOfBinaryTree(root->right);
-        int heights=height(root->left)+height(root->right);
-        return max(leftSideAns,max(rightSideAns,heights));
+        // int leftSideAns=diameterOfBinaryTree(root->left);
+        // int rightSideAns=diameterOfBinaryTree(root->right);
+        // int heights=height(root->left)+height(root->right);
+        // return max(leftSideAns,max(rightSideAns,heights));
+
+        //fastest way to calculate diameter by making some modification in height function
+        modifiedHeight(root);
+        return d;
+
         
     }
 };
