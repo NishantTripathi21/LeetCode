70ef031064ff7c82573255a19c5b71a442908c20
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-13 21:38:47 +0000

    [LeetCode Sync] Runtime - 40 ms (98.81%), Memory - 19.3 MB (100.00%)

--- leetcode-solutions/0632-smallest-range-covering-elements-from-k-lists/solution.cpp
+++ leetcode-solutions/0632-smallest-range-covering-elements-from-k-lists/solution.cpp
@@ -18,16 +18,20 @@ public:
         }
     };
     vector<int> smallestRange(vector<vector<int>>& nums) {
+        // we use min heap and max heap for this question, instead of max heap, we use max keyword
+        //eloborated explanation is written in notebook
         priority_queue<Info,vector<Info>,comp>pq;
         int maxi=INT_MIN;
         int mini=INT_MAX;
         for(int i=0;i<nums.size();i++){
+            //process first element of every array
             int element=nums[i][0];
             Info newInfo(element,i,0);
             pq.push(newInfo);
             maxi=max(maxi,element);
             mini=min(mini,element);
         }
+        //initialise ans
         int ansmax=maxi;
         int ansmin=mini;
         while(!pq.empty()){
@@ -43,6 +47,7 @@ public:
             }
             //insertion 
             if(topCol+1 < nums[topRow].size()){
+                //insert only when each of array is isnt iterated
                 int element=nums[topRow][topCol+1];
                 maxi=max(maxi,element);
                 Info newInfo(element,topRow,topCol+1);
