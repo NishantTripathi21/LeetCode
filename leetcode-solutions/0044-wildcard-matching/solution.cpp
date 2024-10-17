7664614db9fe738502e0d6329a24d9b5a68418ca
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-17 19:13:49 +0000

    [LeetCode Sync] Runtime - 28 ms (85.44%), Memory - 32 MB (33.02%)

--- leetcode-solutions/0044-wildcard-matching/solution.cpp
+++ leetcode-solutions/0044-wildcard-matching/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     bool usingRec(string& s, string& p,int i,int j,vector<vector<int>>&dp){
-        if(i >= s.size() && j>= p.size()){
+        if(i>=s.size() && j>=p.size()){
             return true;
         }
-        if(i>= s.size() && j <= p.size() ){
+        if( i>= s.size() && j <= p.size() ){
+            //if s string ends but p string has charector left to match
             for(int n=j;n<p.size();n++){
-                if(p[n] != '*')return false;
+                if(p[n] != '*'){
+                    return false;
+                }
             }
             return true;
         }
-        if( i <= s.size() && j>=p.size()){
-            return false;
-        }
         if(dp[i][j] != -1)return dp[i][j];
         if(s[i]==p[j] || p[j] == '?'){
-            return dp[i][j]= usingRec(s,p,i+1,j+1,dp);
+            // if one charecter matches 
+            return dp[i][j]=usingRec(s,p,i+1,j+1,dp);
         }
-        if(p[j] =='*'){
-            return dp[i][j]=usingRec(s,p,i+1,j,dp) || usingRec(s,p,i,j+1,dp);
+        if(p[j]=='*'){
+            //if charector of t is star, it can match with any number of elements including zero
+            //i+1,j means star is matched with one charecter of s and called for furthur star matching,
+            //i,j+1 means star matched with no element in s 
+            return dp[i][j] = usingRec(s,p,i+1,j,dp) || usingRec(s,p,i,j+1,dp);
         }
         return false;
     }
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.size(),vector<int>(p.size(),-1));
+        vector<vector<int>>dp(s.size() +1 ,vector<int>(p.size() +1 ,-1));
         return usingRec(s,p,0,0,dp);
     }
 };
