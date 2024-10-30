b11d1c24f2c2b232a8f841aa0c959ae97c302fdc
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-30 18:29:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.5 MB (87.76%)

--- leetcode-solutions/0328-odd-even-linked-list/solution.cpp
+++ leetcode-solutions/0328-odd-even-linked-list/solution.cpp
@@ -10,6 +10,39 @@
  */
 class Solution {
 public:
+    ListNode* helper(ListNode* head){
+        if(!head)return 0;
+        ListNode* curr=head;
+        ListNode* prev=0;
+        ListNode* odd=new ListNode(-1);
+        ListNode*head2=odd;
+        int count=1;
+        while(curr){
+            prev=curr;
+            if(curr->next){
+                curr=curr->next;
+            }
+            else break;
+            count++;
+            
+            if(count % 2==0){
+                prev->next=curr->next;
+                if(!odd){
+                    odd=curr;
+                }
+                else if(odd){
+                    odd->next=curr;
+                    odd=odd->next;
+                    odd->next=0;
+                }
+            }
+            curr=prev->next;
+            count++;
+        }
+        head2=head2->next;
+        prev->next=head2;
+        return head;
+    }
     ListNode* oddEvenList(ListNode* head) {
         if (!head || !head->next) return head;
         ListNode*odd=head;
@@ -23,6 +56,7 @@ public:
         }
         odd->next=head2;
         return head;
+        //return helper(head);
         
     }
 };
