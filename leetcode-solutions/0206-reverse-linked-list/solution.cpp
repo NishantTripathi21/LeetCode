0a2e90aff7815f0e731f9291a807891cd137553a
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-10-30 17:31:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.1 MB (99.99%)

--- leetcode-solutions/0206-reverse-linked-list/solution.cpp
+++ leetcode-solutions/0206-reverse-linked-list/solution.cpp
@@ -11,13 +11,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode*prev=NULL;
+        ListNode* prev=NULL;
         ListNode*curr=head;
-        while(curr!=NULL){
-            ListNode* nextnode=curr->next;
+        while(curr){
+            auto temp=curr->next;
             curr->next=prev;
             prev=curr;
-            curr=nextnode;
+            curr=temp;
         }
         return prev;
     }
