1c82344b313822e9ef316184d337693bb779be3f
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-11-01 21:07:01 +0000

    [LeetCode Sync] Runtime - 3 ms (39.88%), Memory - 185.3 MB (35.08%)

--- leetcode-solutions/0528-swapping-nodes-in-a-linked-list/solution.cpp
+++ leetcode-solutions/0528-swapping-nodes-in-a-linked-list/solution.cpp
@@ -8,6 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ #include<functional>
 class Solution {
 public:
     int FindLen(ListNode*&head){
@@ -19,22 +20,76 @@ public:
         }
         return len;
     }
-    ListNode* swapNodes(ListNode* head, int k) {
-        //kth from end means totalNodes-k+1 from starting
-        if(!head) return NULL;
+    ListNode*changingLinks(ListNode* &head,int k){
+        //this question has many cases:
+        if(!head || !head->next)return head;
         int len=FindLen(head);
-        int last=len-k+1;
-        ListNode*temp1=head;
-        ListNode*temp2=head;
-        while(k!=1){
-            temp1=temp1->next;
-            k--;
+        int lpos=k;
+        int rpos=len-k+1;
+        if(lpos==rpos)return head;
+        //if leftPosition is greater than rightPosiiton
+        if(lpos > rpos)swap(lpos,rpos);
+        if(len==2){
+            //when size of linkedList is 2, and k=1 or 2
+            auto temp=head->next;
+            temp->next=head;
+            head->next=nullptr;
+            head=temp;
+            return head;
         }
-        while(last!=1){
-            temp2=temp2->next;
+        //upto line 60, code for traversal 
+        int last=rpos; 
+        auto startNode=head;
+        auto nextOfStartNode=startNode->next;
+        ListNode*prevOfStartNode=0;
+        auto endNode=head;
+        auto nextOfEndNode=startNode->next;
+        ListNode*prevOfEndNode=0;
+        k=lpos;
+        cout<<"last: "<<last<<" k: "<<k<<endl;
+        while(k!=1){
+            prevOfStartNode=startNode;
+            startNode=nextOfStartNode;
+            nextOfStartNode=nextOfStartNode->next;
+            k--;
+         }
+         while(last != 1){
+            prevOfEndNode=endNode;
+            endNode=nextOfEndNode;
+            nextOfEndNode=nextOfEndNode->next;
             last--;
         }
-        swap(temp1->val,temp2->val);
+        cout<<"rpos: "<<rpos<<" lpos: "<<lpos<<endl;
+        if(lpos==1){
+            //when k==1 or k==length of linked list, head gets changed
+            endNode->next=startNode->next;
+            prevOfEndNode->next=startNode;
+            startNode->next=nextOfEndNode;
+            head= endNode;
+            return head;
+        }
+        if(rpos-lpos==1){
+            // when left position and right position are adjacent
+            prevOfStartNode->next=endNode;
+            endNode->next=startNode;
+            startNode->next=nextOfEndNode;
+            return head;
+        }
+        else{
+            //when leftPosition and rightposition has more than 1 gap
+            cout<<"startNode: "<<prevOfStartNode->val<<" "<<startNode->val<<" "<<nextOfStartNode->val<<endl;
+            cout<<"end node : "<<prevOfEndNode->val<<" "<<endNode->val<<" "<<nextOfEndNode->val<<endl;
+            prevOfStartNode->next=endNode;
+            endNode->next=nextOfStartNode;
+            startNode->next=nextOfEndNode;
+            prevOfEndNode->next=startNode;
+        }
         return head;
+    }
+    ListNode* swapNodes(ListNode* head, int k) {
+        //one method is by swapping node values;
+        //other method is changing links -> invloves multiple cases,which must be handled properly
+        // and seperately->makes it hard level question
+        return changingLinks(head,k);
     }
 };
