18ab4f56425e4d51630f4cdd9a7027cbf0ecd975
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-11-09 22:50:49 +0000

    [LeetCode Sync] Runtime - 25 ms (50.74%), Memory - 137.5 MB (94.89%)

--- leetcode-solutions/0239-sliding-window-maximum/solution.cpp
+++ leetcode-solutions/0239-sliding-window-maximum/solution.cpp
@@ -5,37 +5,26 @@ public:
     deque<int> dq;
     int size = nums.size();
     // first window process till k size
-    for (int i = 0; i < k; i++)
-    {
-        int ele = nums[i];
-        // push elements in the queue and remove chote vale elements
-        while (!dq.empty() && ele > nums[dq.back()])
-        {
+    for(int i = 0; i < k; i++){
+        int element=nums[i];
+        while(!dq.empty() && nums[dq.back()] < element){
             dq.pop_back();
         }
         dq.push_back(i);
     }
-
-    // processs the remaining window Removal and Insertion
-    for (int i = k; i < size; i++)
-    {
-
+    for(int i=k ; i< nums.size(); i++){
+        int element=nums[i];
         ans.push_back(nums[dq.front()]);
-
-        // remove
-        if (i - dq.front() >= k)
+        if( i - dq.front() >= k){
             dq.pop_front();
-            int ele = nums[i];
-        // push elements in the queue and remove chote vale elements
-            while (!dq.empty() && ele > nums[dq.back()])
-            {
+        }
+        while(!dq.empty() && nums[dq.back()] < element){
             dq.pop_back();
-            }
-            dq.push_back(i);
+        }
+        dq.push_back(i);
     }
-    // handle last window
+    //last window
     ans.push_back(nums[dq.front()]);
-
     return ans;
     }
 };
