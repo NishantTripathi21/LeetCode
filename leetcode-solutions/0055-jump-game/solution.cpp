aec9910b7f625950825d042ca763a21a01b1a696
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-11-18 15:51:20 +0000

    [LeetCode Sync] Runtime - 3 ms (28.75%), Memory - 51 MB (99.99%)

---
+++ leetcode-solutions/0055-jump-game/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+bool canJump(vector<int>& nums) {
+    int maxReachable = 0;
+    int n = nums.size();
+    
+    for (int i = 0; i < n; ++i) {
+        if (i > maxReachable) {
+            return false; // Current index is unreachable
+        }
+        maxReachable = max(maxReachable, i + nums[i]);
+        if (maxReachable >= n - 1) {
+            return true; 
+        }
+    }
+    return true;
+    }
+
+};
