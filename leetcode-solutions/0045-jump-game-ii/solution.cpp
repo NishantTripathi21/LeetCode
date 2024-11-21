05f54ebd958551e6076fcd6e65ce6b7f722a698b
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-11-21 18:09:17 +0000

    [LeetCode Sync] Runtime - 359 ms (5.05%), Memory - 21.1 MB (26.68%)

--- leetcode-solutions/0045-jump-game-ii/solution.cpp
+++ leetcode-solutions/0045-jump-game-ii/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int usingRec(vector<int>&nums,int index,vector<int>&dp){
-        if(index == nums.size()-1){
-            return 0;
-        }
+        if( index >= nums.size() -1)return 0;
+        if( nums[index] == 0)return INT_MAX;
         if(dp[index] != -1)return dp[index];
-        int upto=nums[index];
-        int jump=INT_MAX;
-        //=min((int)nums.size()-1-index,upto) becuase many times we were traversing over out of bound array condition
-        for(int i=1;i<=min((int)nums.size()-1-index,upto);i++){
-            int temp=usingRec(nums,i+index,dp);
-            if(temp!=INT_MAX){
-                jump=min(jump,temp+1);
+        int mini=INT_MAX;
+        for( int i = index+1 ; i<= min(index + nums[index], (int)nums.size() -1 ) ;i++){
+            int recAns= usingRec( nums, i,dp);
+            if( recAns != INT_MAX){
+                mini = min( mini ,recAns + 1);
             }
         }
-        return dp[index]=jump;
+        //cout<<" index: "<< index <<" mini: "<< mini<<" ";
+        cout<<endl;
+        return dp[index]= mini;
+
     }
     int jump(vector<int>& nums) {
         vector<int>dp(nums.size()+2,-1);
-        return usingRec(nums,0,dp);
+        return usingRec(nums,0,dp) ;
     }
 };
