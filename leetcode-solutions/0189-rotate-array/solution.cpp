d738f555007ad7f9996fcef5ba290b7f04ed1f14
Nishant Tripathi <nishanttripathi021@gmail.com>
2024-11-29 22:43:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.6 MB (36.41%)

--- leetcode-solutions/0189-rotate-array/solution.cpp
+++ leetcode-solutions/0189-rotate-array/solution.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
+    // void print(vector<int> nums){
+    //     for(auto i:nums){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    void opt(vector<int>& nums, int k){
+        k = k % nums.size();
+        // reverse whole array
+        reverse(nums.begin(), nums.end());
+        // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        // Reverse the remaining n-k elements
+        reverse(nums.begin() + k, nums.end());  
+    }
     void rotate(vector<int>& nums, int k) {
-        vector<int>ans(nums.size());
-
-        for(int i=0;i<nums.size();i++){
-            int newIndex=(i+k)%nums.size();
-            ans[newIndex]=nums[i];
-        }
-        nums=ans;
+        // vector<int>ans(nums.size());
+        // k = k% nums.size();
+        // for(int i=0;i<nums.size();i++){
+        //     int newIndex=(i+k)%nums.size();
+        //     ans[newIndex]=nums[i];
+        // }
+        // nums=ans;
+        opt(nums,k);
         
     }
 };
