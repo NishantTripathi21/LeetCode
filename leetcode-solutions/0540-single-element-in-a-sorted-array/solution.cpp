c35c8df455f19ae22d5694fa35b4db7e3e22ba6e
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-01-14 21:42:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.2 MB (33.59%)

--- leetcode-solutions/0540-single-element-in-a-sorted-array/solution.cpp
+++ leetcode-solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans=ans^nums[i];
+    int method( vector<int>& nums){
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        int s = 1;
+        int e = nums.size()-2;
+        while( s<=e){
+            if( s == e)return nums[s];
+            int mid = s + (e-s)/2;
+            if( nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
+                || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                //eliminate the left half:
+                s = mid + 1;
+            }
+            else {
+                //eliminate the right half:
+                e = mid - 1;
+            }
 
         }
+        return -1;
+    }
+    int singleNonDuplicate(vector<int>& nums) {
+        // int ans=0;
+        // for(int i=0;i<nums.size();i++){
+        //     ans=ans^nums[i];
+
+        // }
         
-        return ans;
+        return method(nums);
         
     }
 };
