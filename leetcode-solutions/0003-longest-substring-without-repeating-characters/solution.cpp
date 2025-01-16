5c363b97f73f660adcd465b439db90265ead5d47
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-01-16 01:56:47 +0000

    [LeetCode Sync] Runtime - 9 ms (53.13%), Memory - 12.1 MB (42.97%)

--- leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int method( string s){
         unordered_map<char, int> charIndexMap;
         int maxLength = 0;
         int left = 0;
-
-        for (int right = 0; right < s.length(); ++right){
-            if(charIndexMap.find(s[right]) != charIndexMap.end() && charIndexMap[s[right]] >= left) {
-                left = charIndexMap[s[right]] + 1;
+        for( int i= 0; i< s.size(); i++){
+            if( charIndexMap.find( s[i]) != charIndexMap.end() && charIndexMap[s[i]] >= left ){
+                left = charIndexMap[s[i]]+1;
             }
-            charIndexMap[s[right]] = right;
-            maxLength = max(maxLength, right - left + 1);
+            charIndexMap[s[i]] = i;
+            maxLength = max( maxLength, i - left + 1);
         }
+        return maxLength;
+    }
+    int lengthOfLongestSubstring(string s) {
+        // unordered_map<char, int> charIndexMap;
+        // int maxLength = 0;
+        // int left = 0;
 
-    return maxLength;
+        // for (int right = 0; right < s.length(); ++right){
+        //     if(charIndexMap.find(s[right]) != charIndexMap.end() && charIndexMap[s[right]] >= left) {
+        //         left = charIndexMap[s[right]] + 1;
+        //     }
+        //     charIndexMap[s[right]] = right;
+        //     maxLength = max(maxLength, right - left + 1);
+        // }
+
+    return method(s);
     }
 };
