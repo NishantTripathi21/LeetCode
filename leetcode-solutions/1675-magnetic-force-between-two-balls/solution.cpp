85febb617ee1598d2cd241f466a5c7b4b7dfc8eb
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-01-22 01:28:10 +0000

    [LeetCode Sync] Runtime - 66 ms (7.93%), Memory - 61.6 MB (67.92%)

---
+++ leetcode-solutions/1675-magnetic-force-between-two-balls/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool possibleToPlace( vector<int>& pos, int m,int dist) {
+        int count = 1;
+        int placed = 0;
+        for( int i = 0; i< pos.size();i++){
+            if( abs(pos[placed] - pos[i]) >= dist){
+                count++;
+                placed = i;
+            }
+            if( count >= m)return true;
+        }
+        return count >= m;
+    }
+    int method( vector<int>& pos, int m) {
+        sort(pos.begin(),pos.end());
+        int low = 0;
+        int high = pos[pos.size()-1] - pos[0];
+        int ans = 0;
+        while( low <= high) {
+            int mid = (low+high)/2;
+            if( possibleToPlace(pos,m,mid)){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
+    int maxDistance(vector<int>& position, int m) {
+        return method(position,m);
+    }
+};
