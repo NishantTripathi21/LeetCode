8a0a30508960f7bf395a01190cca129bf1beccb0
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-01-28 23:28:49 +0000

    [LeetCode Sync] Runtime - 15 ms (74.44%), Memory - 81.2 MB (96.35%)

---
+++ leetcode-solutions/1814-jump-game-vi/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        dq.push_back(0);
+
+        for (int i = 1; i < n; ++i) {
+            if (!dq.empty() && dq.front() < i - k) {
+                dq.pop_front();
+            }
+
+            nums[i] += nums[dq.front()];
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+        return nums[n - 1];
+    }
+};
