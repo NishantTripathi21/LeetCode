9b1c05b67f8df5363b5f161bb94cc0da8d2dcdb4
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-01-29 19:32:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (31.18%)

---
+++ leetcode-solutions/1304-longest-happy-string/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> maxHeap;
+        if (a > 0) maxHeap.push({a, 'a'});
+        if (b > 0) maxHeap.push({b, 'b'});
+        if (c > 0) maxHeap.push({c, 'c'});
+
+        string result = "";
+
+        while (!maxHeap.empty()) {
+            auto [count1, char1] = maxHeap.top();
+            maxHeap.pop();
+            int len = result.size();
+            if (len >= 2 && result[len - 1] == char1 && result[len - 2] == char1) {
+                if (maxHeap.empty()) break; 
+
+                auto [count2, char2] = maxHeap.top();
+                maxHeap.pop();
+
+                //second highest frequency
+                result += char2;
+                if (--count2 > 0) maxHeap.push({count2, char2});
+
+                maxHeap.push({count1, char1});
+            } 
+            else {
+                result += char1;
+                if (--count1 > 0) maxHeap.push({count1, char1});
+            }
+        }
+
+        return result;
+    }
+};
