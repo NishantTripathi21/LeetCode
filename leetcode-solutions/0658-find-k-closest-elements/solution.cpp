e1a57f7867f57a2ed34ac07c4c9f20cbcec25e04
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-02-01 03:29:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 36.4 MB (52.89%)

--- leetcode-solutions/0658-find-k-closest-elements/solution.cpp
+++ leetcode-solutions/0658-find-k-closest-elements/solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-     vector<int> TwoPtrApproach(vector<int>nums,int k,int x){
-        int i=0;int j=nums.size()-1;
+    vector<int> findClosestElements(vector<int>& nums, int k, int x) {
+        int i=0;
+        int j=nums.size()-1;
         while(j-i>=k){
             if(x-nums[i]>nums[j]-x){
                 i++;
@@ -15,9 +16,5 @@ public:
             ans.push_back(nums[b]);
         }
         return ans;
-     }
-
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return TwoPtrApproach(arr,k,x);
     }
 };
