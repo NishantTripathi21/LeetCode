edc3e49a3083b14e27ee2cdfbe0b62c3fe6d4c7f
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-02-07 14:42:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.8 MB (74.09%)

---
+++ leetcode-solutions/1036-rotting-oranges/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int, int>> q;
+        int freshCount = 0, minutes = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+                else if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+        
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!q.empty() && freshCount > 0) {
+            int size = q.size();
+            minutes++;
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (auto [dx, dy] : directions) {
+                    int nx = x + dx, ny = y + dy;
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                        freshCount--;
+                    }
+                }
+            }
+        }
+        
+        return (freshCount == 0) ? minutes : -1;
+    }
+};
