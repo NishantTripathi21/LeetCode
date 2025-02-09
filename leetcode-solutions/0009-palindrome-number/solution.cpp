722b9eb0924391b64f15db3171eb13458ee57b31
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-02-09 12:43:33 +0000

    [LeetCode Sync] Runtime - 3 ms (46.44%), Memory - 8.5 MB (68.50%)

--- leetcode-solutions/0009-palindrome-number/solution.cpp
+++ leetcode-solutions/0009-palindrome-number/solution.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-            // Negative numbers are not palindromes
-                if (x < 0) return false;
-                    
-                        // Variables to store the original number and the reversed number
-                            double original = x, reversed = 0;
-                                
-                                    // Reverse the number
-                                        while (x != 0) {
-                                                int digit = x % 10;
-                                                        reversed = reversed * 10 + digit;
-                                                                x /= 10;
-                                                                    }
-                                                                        
-                                                                            // Check if the original number and the reversed number are the same
-                                                                                return original == reversed;
-
+            if (x < 0) return false;
+            double original = x, reversed = 0;
+            // Reverse the number
+            while (x != 0) {
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x /= 10;
+            }
+        return original == reversed;
     }
 };
