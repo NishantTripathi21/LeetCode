678ae267d85212956cd20bf3ccfa902034a5dfdf
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-03-02 09:50:43 +0000

    [LeetCode Sync] Runtime - 3 ms (41.21%), Memory - 21 MB (22.08%)

--- leetcode-solutions/0547-number-of-provinces/solution.cpp
+++ leetcode-solutions/0547-number-of-provinces/solution.cpp
@@ -1,4 +1,36 @@
 class Solution {
+    class disjoinSet{
+        public:
+        vector<int>parent,size;
+        disjoinSet(int N) {
+            parent.resize(N+1);
+            size.resize(N+1,1);
+            for(int i= 0; i< N+1; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        int findParent(int node) {
+            if( parent[node] == node)return node;
+            return parent[node]= findParent(parent[node]);
+        }
+        void unionBySize(int u, int v) {
+            int Uparent = findParent(u);
+            int Vparent = findParent(v);
+            if( Uparent == Vparent)return;
+
+            if( size[Uparent] < size[Vparent]) {
+                parent[Uparent] = Vparent;
+                size[Vparent] += size[Uparent];
+            }
+            else{
+                parent[Vparent] = Uparent;
+                size[Uparent] += size[Vparent];
+            }
+        }
+    };
+
 public:
     void bfs(int src,unordered_map<int,vector<int>>& adjList , vector<bool>& visited ) {
         queue<int>q;
@@ -23,6 +55,22 @@ public:
             }
         }
     }
+    int usingDSU(vector<vector<int>>& isConnected) {
+        int n =  isConnected.size();
+        disjoinSet ds(n);
+        for(int i= 0; i< n; i++) {
+            for(int j= 0; j< n; j++) {
+                if(isConnected[i][j]) {
+                    ds.unionBySize(i,j);
+                }
+            }
+        }
+        int count = 0;
+        for(int i= 0; i< ds.parent.size(); i++) {
+            if(ds.parent[i] == i)count++;
+        }
+        return count-1;
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         // make adjecency list
         unordered_map<int,vector<int>> adjList;
@@ -44,7 +92,7 @@ public:
                 count++;
             }
         }
-        return count;
+        return usingDSU(isConnected);
 
     }
 };
