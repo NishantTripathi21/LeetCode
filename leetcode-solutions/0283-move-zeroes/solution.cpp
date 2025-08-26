f564085581b6ba2aff66f28bc06c305ab1c5a2ef
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-08-26 03:31:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.8 MB (81.65%)

--- leetcode-solutions/0283-move-zeroes/solution.cpp
+++ leetcode-solutions/0283-move-zeroes/solution.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int a = 1;
-        int b = 0; 
-        while(b < nums.size() && nums[b] != 0){
-            b++;
+        // int a = 1;
+        // int b = 0; 
+        // while(b < nums.size() && nums[b] != 0){
+        //     b++;
+        // }
+        // if(b==nums.size()-1)return;
+        // a = b + 1;
+        // while( a < nums.size()){
+        //     if( nums[a] != 0){
+        //         swap(nums[a],nums[b]);
+        //         b++;
+        //     }
+        //     a++;
+        // }
+        int i = 0;
+        int j = 0;
+        while( i < nums.size() && nums[i] != 0) {
+            i++;
         }
-        if(b==nums.size()-1)return;
-        a = b + 1;
-        while( a < nums.size()){
-            if( nums[a] != 0){
-                swap(nums[a],nums[b]);
-                b++;
+        if(i == nums.size()-1)return;
+        j = i + 1;
+        while(j < nums.size()) {
+            if(nums[j] != 0) {
+                swap(nums[i],nums[j]);
+                i++;
             }
-            a++;
+            j++;
         }
+        return;
     }
+    
+    
+
 };
