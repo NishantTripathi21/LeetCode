d3ef8a8473de25cb60e1de65e990e6ba1dec4e78
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-08-26 07:30:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.7 MB (11.91%)

--- leetcode-solutions/0075-sort-colors/solution.cpp
+++ leetcode-solutions/0075-sort-colors/solution.cpp
@@ -1,82 +1,24 @@
 class Solution {
 public:
-    vector<int> bruteforce(vector<int>&nums){
-        int size=nums.size();
-        int ones=0;
-        int twos=0;
-        int threes=0;
-        for(int i=0;i<size;i++){
-            if(nums[i]==0){
-                ones++;
-            }
-           else if(nums[i]==1){
-                twos++;
-            }
-           else if(nums[i]==2){
-                threes++;
-            }
-        }
-        int n=0;
-        while(ones--){
-            nums[n]=0;
-            n++;
-        }
-         while(twos--){
-            nums[n]=1;
-            n++;
-        }
-         while(threes--){
-            nums[n]=2;
-            n++;
-        }
-          return nums;  
-            }
- 
-    vector<int> threePointerApproach(vector<int>&nums){
-        int mid,low,high;
-        mid=low=0;
-        high=nums.size()-1;
-        while(high>=mid){
-            if(nums[mid]==0){
-                swap(nums[mid],nums[low]);
-                mid++;
-                low++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            }
-            else{
-                swap(nums[mid],nums[high]);
-                high--;
-            }
-        }
-        return nums;
-         }
-
-    vector<int>swappingapproach(vector<int>&nums){
-        int index,low,high;
-        index=0;low=0;
-        high=nums.size()-1;
-        while(index<=high){
-            if(nums[index]==0){
-                swap(nums[index],nums[low]);
-                low++;
-                index++;
-            }
-            else if(nums[index]==2){
-                swap(nums[index],nums[high]);
-                high--;
-             }
-             else{
-                index++;
-             }
-        }
-        return nums;
-    }
-
     void sortColors(vector<int>& nums) {
-      // bruteforce(nums);
-      //  threePointerApproach(nums);
-      swappingapproach(nums);
+        int i= 0;
+        int j = nums.size()-1;
+        int index= 0;
+        while( i < nums.size() && nums[i]==0) {
+            i++;
+        }
+        index = i ;
+        while(index < nums.size() && i < j) {
+            if(nums[index] == 0) {
+                swap(nums[index],nums[i]);
+                i++;
+                index++;
+            }
+            else if(nums[index] == 2 && index < j) {
+                swap(nums[index],nums[j]);
+                j--;
+            }
+            else index++;
+        } 
     }
-};
+}; 
