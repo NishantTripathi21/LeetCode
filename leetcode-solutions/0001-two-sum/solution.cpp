17288b98528e65a868604ebb0e3e20a0ca0fdde9
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-08-26 04:11:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.9 MB (37.06%)

--- leetcode-solutions/0001-two-sum/solution.cpp
+++ leetcode-solutions/0001-two-sum/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap; // To store the number and its index
-
+        unordered_map<int, int> numMap; 
         for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i]; // Find the complement of the current number
-
+            int complement = target - nums[i]; 
             if (numMap.find(complement) != numMap.end()) {
-                // If complement exists in the map, we found the solution
                 return { numMap[complement], i };
             }
-            // Store the current number and its index in the map
             numMap[nums[i]] = i;
         }
         return {};
-        
     }
+
 };
