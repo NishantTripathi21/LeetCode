392177b2b15615a77325c1063d8a2692e1e56eb7
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-09-25 14:45:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (94.48%)

--- leetcode-solutions/0120-triangle/solution.cpp
+++ leetcode-solutions/0120-triangle/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int usingRec(vector<vector<int>>& triangle,int x,int y,vector<vector<int>>&dp){
-        if(x==triangle.size()-1){
-            return triangle[x][y];
-        }
-        if(dp[x][y] != -1)return dp[x][y];
-        int firstMove=INT_MAX;
-        if( true){
-            firstMove=triangle[x][y]+ usingRec(triangle,x+1,y,dp);
-        }
-        int secondMove=INT_MAX;
-        if( true){
-            secondMove=triangle[x][y]+usingRec(triangle,x+1,y+1,dp);
-        }
-        return dp[x][y]=min(firstMove,secondMove);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(triangle.size(),vector<int>(triangle[triangle.size()-1].size(),-1));
-        return usingRec(triangle,0,0,dp);
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+
+                int below = triangle[i + 1][j];
+                int belowRight = triangle[i + 1][j + 1];
+
+                triangle[i][j] += min(below, belowRight);
+
+            }
+        }
+
+        return triangle[0][0];
     }
 };
