c9ffb90cd5f5baf35dddc1bceabb03f5cbd2bbdc
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-10-14 13:18:48 +0000

    [LeetCode Sync] Runtime - 10 ms (18.14%), Memory - 18.4 MB (89.94%)

---
+++ leetcode-solutions/2816-lexicographically-smallest-palindrome/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+        while(left < right){
+            if(s[left] <= s[right]){
+                s[right] = s[left];
+            }
+            else s[left] = s[right];
+            left++;
+            right--;
+        }
+
+        return s;
+    }
+};
