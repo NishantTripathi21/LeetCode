c3cc6db01720b7b2774a27a6238f8a424606d370
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-10-14 15:01:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.9 MB (32.52%)

--- leetcode-solutions/1850-minimum-length-of-string-after-deleting-similar-ends/solution.cpp
+++ leetcode-solutions/1850-minimum-length-of-string-after-deleting-similar-ends/solution.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int sol( string s){
-        int n = s.size();
-        int i = 0;
-        int j = n - 1;
-        while( i < j && s[i] == s[j]){
-            while( i< j-1 && s[i] == s[j] && s[i] == s[i+1]) {
-                i++;
-            }
-            while( j > i+1 && s[j] == s[i] && s[j] == s[j-1]){
-                j--;
-            }
-            i++;
-            j--;
-            // cout<<" i: "<<i<<" ";
-            // cout<<" j: "<<j<<" "<<endl;
-        }
-        return j - i + 1;
-    }
     int minimumLength(string s) {
-        return sol(s);
+        int left = 0, right = s.size() - 1;
+        
+        while(left < right) {
+            if(s[left] != s[right]) break;
+            
+            char ch = s[left];
+            while(left <= right && s[left] == ch) left++; 
+            while(left <= right && s[right] == ch) right--;
+        }
+        
+        return right - left + 1;
     }
 };
+
