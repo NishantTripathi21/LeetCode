f0c5a10d637cb0cb7afa80c9a26fa63f1f747cf0
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-10-15 13:52:42 +0000

    [LeetCode Sync] Runtime - 28 ms (7.85%), Memory - 18.8 MB (7.68%)

--- leetcode-solutions/3195-separate-black-and-white-balls/solution.cpp
+++ leetcode-solutions/3195-separate-black-and-white-balls/solution.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    long long method( string s){
-        int n = s.size();
-        if( n < 2)return 0;
-        int i = 0;
-        //int zeros = 0;
-        long long swaps = 0;
-        long long ones = 0;
-        
-        while( i < n){
-            if( s[i] == '1'){
-                ones++;
-            }
-            else{
-                swaps += ones*1; 
-            }
-            i++;
-        }
-        return swaps;
-        
-    }
     long long minimumSteps(string s) {
-        return method(s) ;
+        long long count = 0;
+        string t = s;
+        sort(t.begin(),t.end());
+
+        int i= s.size()-1 , j = s.size()-1;
+
+        while( i >= 0){
+            while(i >= 0 && s[i] == '0') i--;
+            if(i < 0) break;
+            count += 1LL* (j - i);
+            i--;
+            j--;
+        }
+
+        return count;
     }
 };
