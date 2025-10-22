434778f77a234eac83f4b90b4969417d80ef3f58
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-10-22 09:20:31 +0000

    [LeetCode Sync] Runtime - 232 ms (17.43%), Memory - 156.2 MB (11.98%)

--- leetcode-solutions/2868-continuous-subarrays/solution.cpp
+++ leetcode-solutions/2868-continuous-subarrays/solution.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        deque<int>maxiWindow,miniWindow; 
+        multiset<int>track;
         int left = 0;
         int right = 0;
-        long long subarrays = 0;
+        long long count = 0;
         while( right < nums.size()) {
-            int element = nums[right];
-            while(!maxiWindow.empty() && nums[maxiWindow.back()] <= element) {
-                maxiWindow.pop_back();
-            }
-            while(!miniWindow.empty() && nums[miniWindow.back()] >=element) {
-                miniWindow.pop_back();
-            }
-            maxiWindow.push_back(right);
-            miniWindow.push_back(right);
-            int mini = nums[miniWindow.front()];
-            int maxi = nums[maxiWindow.front()];
-            if( abs(mini-maxi) > 2) {
+            int val = nums[right];
+            track.insert(val);
+            while( abs((*track.begin())- (*track.rbegin())) > 2 ) {
+                int tr = nums[left];
+                auto ptr = track.find(tr);
+                track.erase(ptr);
                 left++;
-                if( !maxiWindow.empty() && maxiWindow.front() < left){
-                    maxiWindow.pop_front();
-                }
-                if( !miniWindow.empty() && miniWindow.front() < left){
-                    miniWindow.pop_front();
-                }
             }
-            else{
-                subarrays += (long long) right-left+1;
-                right++;
-            }
-            //cout<<" left: "<<left<<" right: "<<right<<" mini: "<<mini<<" maxi: "<<maxi<<" subarrays: "<<subarrays<<endl;
+            count += (long long)(right-left) + 1;
+            right++;
         }
-        return subarrays;
+        return count;
     }
 };
