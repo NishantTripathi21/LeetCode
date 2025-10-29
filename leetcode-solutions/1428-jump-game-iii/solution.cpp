7962949bc436625546cf959b9ea4a94fc65b9256
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-10-29 17:36:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 36.7 MB (56.25%)

--- leetcode-solutions/1428-jump-game-iii/solution.cpp
+++ leetcode-solutions/1428-jump-game-iii/solution.cpp
@@ -3,22 +3,20 @@ public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         vector<int>vis(n,0);
-        queue<int>q; // index is pushed
-        q.push(start);
         vis[start] = 1;
+        queue<int>q;
+        q.push(start);
         while(!q.empty()) {
             int index = q.front();
-            q.pop();
             if(arr[index] == 0)return true;
-
-            if(index - arr[index] >= 0 && vis[index-arr[index]] == 0){
+            q.pop();
+            if( index - arr[index] >= 0 && !vis[index-arr[index]] ) {
                 q.push(index-arr[index]);
-                vis[index-arr[index]] = 1;
+                vis[index-arr[index]]= 1;
             }
-
-            if(index + arr[index] < n && vis[index+arr[index]] == 0) {
+            if( index + arr[index] < n && !vis[index+arr[index]] ) {
                 q.push(index+arr[index]);
-                vis[index+arr[index]] = 1;
+                vis[index+arr[index]]= 1;
             }
         }
         return false;
