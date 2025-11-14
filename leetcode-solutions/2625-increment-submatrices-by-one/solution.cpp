5b0595cf6d9f27d9c716c58d9629daced5aba1bf
Nishant Tripathi <nishanttripathi021@gmail.com>
2025-11-14 14:12:01 +0000

    [LeetCode Sync] Runtime - 59 ms (29.58%), Memory - 91.5 MB (45.56%)

--- leetcode-solutions/2625-increment-submatrices-by-one/solution.cpp
+++ leetcode-solutions/2625-increment-submatrices-by-one/solution.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     vector<vector<int>> method1( int n, vector<vector<int>>& queries) {
         vector<vector<int>> ans( n, vector<int>(n,0));
-        // Iterate through each query to mark the increments in the grid.
         for( auto i: queries){
             int r1 = i[0];
             int r2 = i[2];
             int c1 = i[1];
             int c2 = i[3];
-            // Increment the starting column for each row in the range [r1, r2].
-            for ( int r = r1; r<= r2; r++){
-                ans[r][c1]++;
-                // Decrement the element right after the ending column, if within bounds.
+            for ( int r = r1; r<= r2; r++) {
+                ans[r][c1]++; 
                 if( (c2+1) < n)ans[r][c2+1]--;
             }
         }
-        // Propagate the increments horizontally to apply the range increment effect.
         for( int i = 0; i< n; i++){
             for( int j = 1; j< n; j++){
                 ans[i][j] += ans[i][j-1];
